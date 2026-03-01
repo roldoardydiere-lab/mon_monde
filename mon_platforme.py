88fbbc6b9b76e103603baf764fa3e670f4b354f8
Roldo <roldoardydiere@gmail.com>
2026-03-01 17:34:37 +0100

    ajoute message vu

--- mon_platforme.py
+++ mon_platforme.py
@@ -1,62 +1,59 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_socketio import SocketIO, emit
-from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
-from database import init_db
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.secret_key = "secret"
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-init_db()
+# dictionnaire pour stocker pseudo et couleur
+users = {}
+colors = ["blue", "green", "red", "orange", "purple", "brown"]
 
-def get_db():
-    return sqlite3.connect("chat.db")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        user = request.form["username"]
-        pwd = request.form["password"]
-
-        db = get_db()
-        c = db.cursor()
-        c.execute("SELECT password FROM users WHERE username=?", (user,))
-        row = c.fetchone()
-
-        if row and check_password_hash(row[0], pwd):
-            session["user"] = user
-            return redirect("/chat")
-
-    return render_template("login.html")
-
-@app.route("/register", methods=["POST"])
-def register():
-    user = request.form["username"]
-    pwd = generate_password_hash(request.form["password"])
-
-    db = get_db()
-    c = db.cursor()
-    c.execute("INSERT OR IGNORE INTO users (username, password) VALUES (?,?)", (user, pwd))
-    db.commit()
-    return redirect("/")
-
-@app.route("/chat")
-def chat():
-    db = get_db()
-    c = db.cursor()
-    c.execute("SELECT username, text FROM messages")
-    messages = [{"username": u, "text": t} for u, t in c.fetchall()]
-    return render_template("chat.html", username=session.get("user"), messages=messages)
+@socketio.on('join')
+def on_join(data):
+    sid = request.sid
+    pseudo = data['pseudo']
+    color = colors[len(users) % len(colors)]
+    users[sid] = {'pseudo': pseudo, 'color': color}
+    send(f"{pseudo} a rejoint le chat", broadcast=True)
 
 @socketio.on("send_message")
 def handle_message(data):
-    db = get_db()
-    c = db.cursor()
-    c.execute("INSERT INTO messages (username, text) VALUES (?,?)",
-              (data["username"], data["text"]))
-    db.commit()
     emit("receive_message", data, broadcast=True)
+    emit("message_send", {"username": data["username"]}, broadcast=True)
+
+@socketio.on("message_seen", function(data) {
+    if( data.username === username) {
+        const messages = document.querrySelectorAll("right");
+        if(messages.length  ) {
+            const last = message[message.length - 1];
+            if(! last.querySelector(".seen")) {
+                cont seen = document.createElement("div");
+                seen.className = "seen";
+                seen.textContent = ""
+
+        }
+    }
+})
+
+@socketio.on('disconnect')
+def on_disconnect():
+    sid = request.sid
+    user = users.pop(sid, {'pseudo': 'Inconnu'})
+    send(f"{user['pseudo']} a quitté le chat", broadcast=True)
+
+import eventlet
+eventlet.monkey_patch()
+
+import os
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
